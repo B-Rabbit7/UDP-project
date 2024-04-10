d2b7fed1942c081ab6a6166f56ecfe33ec836623
B-Rabbit7 <snouri3@my.bcit.ca>
2024-04-09 17:37:14 -0700

    fixed basic proxy conn

--- proxy.py
+++ proxy.py
@@ -7,20 +7,24 @@ PROXY_PORT = 8888
 
 # Server configuration
 SERVER_IP = "127.0.0.1"
-SERVER_PORT = 5555
+SERVER_PORT = 8889
 
 # Client configuration
 CLIENT_IP = "127.0.0.1"
 CLIENT_PORT = 8080
 
+
 def handle_client_to_server(client_data, server_socket):
     server_socket.sendto(client_data, (SERVER_IP, SERVER_PORT))
+
 
 def handle_server_to_client(client_socket, server_data):
     client_socket.sendto(server_data, (CLIENT_IP, CLIENT_PORT))
 
+
 # Handle client function definition
-def handle_client(client_socket, client_addr):
+def handle_client(client_socket, client_data):
+    print(client_data)
     try:
         # Create server socket
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,6 +34,7 @@ def handle_client(client_socket, client_addr):
             client_data, _ = client_socket.recvfrom(1024)
             if not client_data:
                 break
+            print("Received message: {}".format(client_data))
 
             # Forward data to server
             handle_client_to_server(client_data, server_socket)
@@ -43,6 +48,7 @@ def handle_client(client_socket, client_addr):
     except Exception as e:
         print(f"Error: {e}")
 
+
 # Main function
 def main():
     # Create proxy socket
@@ -55,16 +61,19 @@ def main():
         while True:
             # Accept client connection
             client_data, client_addr = proxy_socket.recvfrom(1024)
+            proxy_socket.sendto(client_data, (SERVER_IP, SERVER_PORT))
             print(f"Accepted connection from client: {client_addr}")
+            print(f"Received data from client: {client_data}")
 
             # Handle client in a separate thread
-            client_thread = threading.Thread(target=handle_client, args=(proxy_socket, client_addr))
-            client_thread.start()
+            # client_thread = threading.Thread(target=handle_client, args=(proxy_socket, client_data))
+            # client_thread.start()
 
     except KeyboardInterrupt:
         print("Proxy shutting down...")
     finally:
         proxy_socket.close()
+
 
 if __name__ == "__main__":
     main()
